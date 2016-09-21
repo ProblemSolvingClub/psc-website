0fb53e025b442638737e6f2e1cacd988b15efb0b
Jan Clarin <hi@janclarin.com>
2016-09-21 01:48:32 -0600

    add leaderboard page

---
+++ leaderboard/leaderboard.controller.js
@@ -0,0 +1,41 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('leaderboard')
+        .controller('LeaderboardController', LeaderboardController);
+
+    LeaderboardController.$inject = ['leaderboardService'];
+
+    function LeaderboardController(leaderboardService) {
+        var vm = this;
+        vm.getUserDisplayName = getUserDisplayName;
+        vm.getTier = getTier;
+        vm.users = [];
+        vm.sites = [];
+        vm.tiers = [];
+
+        leaderboardService.get()
+            .then(function(response) {
+                vm.users = response.users;
+                vm.sites = response.sites;
+                vm.tiers = response.tiers;
+            });
+
+        function getUserDisplayName(user) {
+            var displayName = user.firstName;
+            if (user.lastName) {
+                displayName += ' ' + user.lastName.charAt(0) + '.';
+            }
+            return displayName;
+        }
+
+        function getTier(totalSolved) {
+            for (var i = vm.tiers.length - 1; i >= 0; i--) {
+                if (totalSolved > vm.tiers[i].minimumScore) {
+                    return vm.tiers[i];
+                }
+            }
+        }
+    }
+})();
---
+++ leaderboard/leaderboard.service.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('leaderboard')
+        .factory('leaderboardService', leaderboardService);
+
+    leaderboardService.$inject = ['$http'];
+
+    function leaderboardService($http) {
+        var apiUrl = '/ranking.php';
+
+        return {
+            get: get
+        };
+
+        function get() {
+            return $http.get(apiUrl)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+    }
+})();
