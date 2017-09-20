7bd122c630b0667cab8def5c1699a1a96c7ee702
Martin Tran <martin.hua.tran@gmail.com>
2017-09-20 16:22:45 -0600

    Replaced current practice contest link with link to Wednesday problem solutions page.

---
+++ solutions/R2.java
@@ -0,0 +1,23 @@
+/*
+ Author: Martin Tran
+ Email: martin.tran@ucalgary.ca
+
+ Feel free to send any questions about this problem
+ to the email above or ask in the CPC discord.
+ */
+import java.util.Scanner;
+
+public class R2 {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int r1 = sc.nextInt();
+	int s = sc.nextInt();
+	sc.close();
+	
+	int r2 = 2 * s - r1;
+	
+	System.out.println(r2);
+    }
+}
