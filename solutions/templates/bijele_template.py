7f12967088d4400dfb34e811aaf19246d7f0e278
Martin Tran <martin.hua.tran@gmail.com>
2017-09-27 17:35:49 -0600

    added page for solution templates

---
+++ solutions/templates/bijele_template.py
@@ -0,0 +1,23 @@
+# Only use one of the two code blocks! Comment out the one you're not using.
+
+
+
+
+# How to start by using individual variables.
+
+K, Q, R, B, N, P = [int(i) for i in input().split()]
+print("Number of Kings is:", K)
+print("Number of Queens is:", Q)
+print("Number of Rooks is:", R)
+print("Number of Bishops is:", B)
+print("Number of Knights is:", N)
+print("Number of Pawns is:", P)
+
+
+
+
+# How to start by using a list. (Harder)
+
+# myList = [int(i) for i in input().split()]
+# for index, element in enumerate(myList):
+#     print("Index:", index, "Element:", element)
