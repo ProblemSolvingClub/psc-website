5cc6ba934ce315ae67f107ec178c782f36af69fd
martin.tran <martin.tran@ucalgary.ca>
2018-03-15 20:20:01 -0600

    updated 2018 contestants & draw now automatically stops after pressing

---
+++ contests/ccpc/2018/draw.js
@@ -0,0 +1,53 @@
+var officialContestants = "Sharon,Wang, danknamic\nRaza,Qazi, danknamic\nAlex,Bymoen, danknamic\nAlan,Fung, No Limit\nMuhammad,Hassan, NullPointerException\nBea,Esguerra, cookies\nBrad,Pfannmuller, cookies\nChris,Sun, Not Citrus\nEric,Ho, Not Citrus\nJohn,Ngo, Not Citrus\nStruan,Wood, blackbox\nPhillip,Renwick, blackbox\nJohn,Kim, blackbox\nParker,Wong, xQcismyhero\nJeffrey,Tong, xQcismyhero\nFrankie,Chen, xQcismyhero\nTyrell,Jaques, \nAnna,Tran, Uchuujin\nDante,Bencivenga, Fourth Place\nBrian,Pho, Leaderboard[-1]\nNicolas,Gonzalez, \nWesley,Tang, TheNullPointers\nKeenan,Gaudio, TheNullPointers\nSunny,Nagam, TheNullPointers\nHadiya,Firdaus, YOPO\nDebbie,Macrohon, YOPO\nJoanna,Huynh, YOPO\nRobert,Dumitru, Swimming Respecters\nDominic,Demierre, Swimming Respecters\nMichael,Han, \nBradley,Ritten, //TODO: Team Name\nChristilyn,Arjona, //TODO: Team Name\nAnton,Lysov, The Cowboy Coders\nChristopher,Behan, The Cowboy Coders\nChristian,Velasco, christian.velasco@ucalgary.ca\nBrody,Jackson, christian.velasco@ucalgary.ca\nRyan,Doering, christian.velasco@ucalgary.ca\nAnastasiya,Lazarenko, \nJustin,Hung, Ankle Byters\nMagnus,Lyngberg, Ankle Byters\nRen,Xu, Ankle Byters\nRobert,Fiker, \nVictor,Zheng, https://victorz.ca\nYashharan,Babra, this.team\nYesha,Patel, this.team\nDevon,Vipond, StackOverflow\nAklas,Cheema, \nReginald,Lybbert, \nDuncan,Campbell, \nMohit,Parmar, \"Hello(Goodbye)World!\"\nJadon,Duby, \"Hello(Goodbye)World!\"\nAlbert,Choi, \"Hello(Goodbye)World!\"\nRyan,Der, StackOverflow\nRohit,Kadhe, StackOverflow\nNicole,Caballero, this.team\nLiam,Wrubleski, The All-American Regex\nMarissa,Baden, The All-American Regex\nKhesualdo,Condori, \nAlbert,Luu, 3 Guys 1 Comp\nBrandon,Coombe, \nNiran,Pon, Espresso\nDuncan,Lam, how do we program\nHansel,Qiu, how do we program\nNathanael,Cheung, how do we program\nJanam,Marthak, Recursive Raptors\nSafi,Cheema, Recursive Raptors\nBryce,Besler, Liberal Arts Majors\nAkash,Melethil, Liberal Arts Majors\nChenlei(iris),Bao, Dracarys\nZachariah,Albers, Dracarys\nTina,Huynh, Sexidecimals\nOre,Arowobusoye, Sexidecimals\nDavid,Sepulveda, Sexidecimals\nBritta,Den,hoed, Espresso\nThomas,Vy, M'engineers\nRainer,Lim, M'engineers\nJustin,Zhou, Reliable Coffee Engineering\nKevin,Chow, Reliable Coffee Engineering\nDerek,Braun, Reliable Coffee Engineering\nDomenic,Bruno, Winner Winner Chicken Dinner\nJeremy,Bilic, Winner Winner Chicken Dinner\nJose,Galeano, Winner Winner Chicken Dinner\nCameron,Edwards, F-Triple Minus\nJordon,Cheung, F-Triple Minus\nNian,He, F-Triple Minus\nStefan,Jovanovic, \nSean,Ovens, LilRed&TheBigBadVolf\nBlake,Mealey, Longs 101\nHenry,Stoldt, Double Decker Couch\nZeyu,Liu, Double Decker Couch\nInderpreet,Dhillon, ExceptionalExceptions\nBrandon,Khoo, ExceptionalExceptions\nDavid,Nguyen, ExceptionalExceptions\nJacob,Cuke, Dracarys\nCe,Zheng, Bullfrogs\nAlex,Liu, Bullfrogs\nZachary,Lau, Bullfrogs\nAndrew,Heng, 3 Guys 1 Comp\nMaaz,Khurram, \nJoweria,Ekram, \nMartin,Tran, 2D > 3D\nJenny,Le, Cache Me Ousside How Bow Dah\nSpencer,Manzon, Cache Me Ousside How Bow Dah\nFarrah,Urmeneta, Cache Me Ousside How Bow Dah\nParva,Thaker, Null pointer\nBader,Abdulwaseem, Null pointer\nAdrian,Bathan, Lily White\nManjeet,Dev, willie's willy\nWillie,Li, willie's willy\nMatt,Cruz, willie's willy\nMichael,Svoboda, Genesys\nMd,Rashik,hassan, Genesys\nVlad,Chestnykh, Espresso\nOscar,Wong, Triple Axel\nKevin,Ta, Triple Axel\nBehnam,Ousat, Nerquad\nAmirali,Kazeminejad, Nerquad\nSeyed,nima,Miryeganeh, Nerquad\nBen,Roberts, \nKent,Wong, \nAhmad,Hassan, Area 51\nJuan,luis,De,reiset,jimenez, Area 51\nBryan,Huynh, Team Prowse\nJeffery,Yang, Team Prowse\nJacky,Zhou, Team Prowse\nDaniel,Dastoor, Longs 101\nNathan,Godard, N+J\nJathniel,Ong, N+J\nKendra,Wannamaker, LilRed&TheBigBadVolf\nBrian,Kehrig, PyBrains\nThomas,Vu, Lily White\nTeresa,Van, Lily White\nEric,ma,Ma, Wahaj\nMicheal,Friesen, Wahaj\nChris,Kinzel, Wahaj\nJuan,Nieto, Its not a bug, its a feature\nMaria,Lau, Its not a bug, its a feature\nBailey,Duncan, Its not a bug, its a feature\nWilliam,Hong, \nXincheng,Shen, Double Decker Couch\nElodie,Boudes, IllegalSkillsException\nChristopher,Smith, IllegalSkillsException\nSara,Li, TEAM KETCHUP\nBill,Luu, TEAM KETCHUP\nNathanael,Huh, TEAM KETCHUP\nPaul,Dan, IllegalSkillsException\nJacob,Goodwin, TheTrendyProgrammers\nAndrew,Burton, TheTrendyProgrammers\nAdarsh,Melethil, No Limit\nBranko,Bajic, \nMichael,Hoang, ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿\nDuc,Do, ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿\nSophia,Amin, \nJohn,Anglo, \nToshi,Taperek, \nKarla,Juego, Camp Half-Bits\nAtika,Syeda, Camp Half-Bits\nQiyue,Zhang, LetsVan\nYichen,Ding, LetsVan\nXi,Wang, LetsVan\nMunib,Rahman, \nShanty,Kamal, \nHager,Ben,mansour";
+
+var ofSplit = officialContestants.split("\n");
+var ofArr = [];
+var winners = [];
+for (var i = 0; i < ofSplit.length; i++) {
+    var contestant = ofSplit[i].split(",");
+    ofArr.push([contestant[1], contestant[0], contestant[2]]);
+}
+ofArr.sort(function(a, b) {
+    if (a[1] < b[1]) return -1;
+    if (a[1] == b[1]) return a[0] < b[0] ? -1 : (a[0] > b[0] ? 1 : 0);
+    return 1;
+});
+var index;
+
+function writeContestants() {
+    var html_to_write = "";
+    for (var i = 0; i < ofArr.length; i++)
+	html_to_write += ofArr[i][1] + " " + ofArr[i][0] + ", ";
+    document.getElementById("remaining_contestants").innerHTML = html_to_write;
+}
+
+function draw() {
+    var button = document.getElementById("button");
+    button.disabled = true;
+    button.value = "And the winner is..."
+    var roulette = setInterval(
+	function() {
+	    index = Math.floor(Math.random() * (ofArr.length));
+	    document.getElementById("winner").innerHTML = ofArr[index][1] + " " + ofArr[index][0];
+	    document.getElementById("team").innerHTML = ofArr[index][2];
+	},
+	50);
+
+    setTimeout(
+	function() {
+	    clearInterval(roulette);
+	    index = Math.floor(Math.random() * (ofArr.length));
+	    document.getElementById("winner").innerHTML = ofArr[index][1] + " " + ofArr[index][0];
+	    document.getElementById("team").innerHTML = ofArr[index][2];
+	    ofArr.splice(index, 1);
+	    document.getElementById("contestant_count").innerHTML = "List of official contestants (" + ofArr.length.toString() + ")";
+	    writeContestants();
+	    document.getElementById("button").disabled = false;
+	    document.getElementById("button").value = "Start Draw";
+	},
+	3000);
+}
+document.getElementById("button").addEventListener("click", draw);
+
+writeContestants();
+document.getElementById("contestant_count").innerHTML = "List of official contestants (" + ofArr.length + "):";
