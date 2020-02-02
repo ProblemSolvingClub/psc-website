73f30b1d34650feaca79dbba1430d549a031bde2
jasond-n <Jason_D_N@hotmail.com>
2020-02-01 20:22:26 -0700

    I have created the CCPC 2020 folder

---
+++ contests/ccpc/2020/draw.js
@@ -0,0 +1,53 @@
+var officialContestants = "Julien, Lam, ̓̽̍ ̾̽̇ ͗ͤ̏ ̇ͤ͊҉̻͓̤ ̉ͥ̋ ͤ̓̑ ͑̈́̐ ͨ̈̚ ̀͒̚ ̾̄͊҉͓̀͞ ̈́͂͆ ̃͐̑\nYifeng, Pan, ̓̽̍ ̾̽̇ ͗ͤ̏ ̇ͤ͊҉̻͓̤ ̉ͥ̋ ͤ̓̑ ͑̈́̐ ͨ̈̚ ̀͒̚ ̾̄͊҉͓̀͞ ̈́͂͆ ̃͐̑\nAndy, Wu, A Bug's Life\nWaley, Chen, A Bug's Life\nIvan, Ristanen, Aether\nMatthew, Wojcik, Aether\nZia, Rehman, Aether\nAlykhan, Hassam, Alykhan Hassam\nFrankie, Chen, Atago\nJeffrey, Tong, Atago\nParker, Wong, Atago\nAustin, So, Austin So\nEthan, Kerr, BickleBlatoon\nUmut, Emre, BickleBlatoon\nXinhua, Cao, BickleBlatoon\nCole, Adams, Big Data Energy\nSunny, Nagam, Big Data Energy\nWesley, Tang, Big Data Energy\nBraydon, Willms, Braydon Willms\nCe, Zheng, Bullfrogs\nTejus, Gangadharaiah, Bullfrogs\nZachary, Lau, Bullfrogs\nAmirali, Kazeminejad, CalTeam\nMohammad-Jafar, Mashhadi-Ebrahim, CalTeam\nseyed, nima-miryeganeh, CalTeam\nBrenton, Kruger, Choo choo\nMichaela, Olsakova, Choo choo\nMicheal, Friesen, Choo choo\nIssack, John, cmd\nDante, Bencivenga, Confidently Composing Practical Code\nKendra, Wannamaker, Confidently Composing Practical Code\nCalvin, Dang, Da Bob\nChenlei(Iris), Bao, Da Bob\nZhenkun, Hu, Da Bob\nDaanish, Mazhar, Daanish Mazhar\nDaniil, Pleskach, DaniilNFrens\nMatthew, Lee, DaniilNFrens\nMinh, Lam, DaniilNFrens\nDeclan, Quinn, Double Decker Couch\nGraham, Doerksen, Double Decker Couch\nHenry, Stoldt, Double Decker Couch\nDylan, Wheeler, Dylan Wheeler\nEdward, Song, Edward Song\nCameron, Edwards, F-Triple-Minus\nJordon, Cheung, F-Triple-Minus\nToshi, Taperek, F-Triple-Minus\nJia-Gang, Chang, Free\nJinseo, Lee, Free\nGary, Li, Gary Li\nGeorge, Abouseta, George Abouseta\nAnshil, Gandhi, Goldfish\nGuransh, Mangat, Guransh Mangat\nAidan, Prior, hasinsqualitymaleharamservice.weebly.com\nHasin, Zaman, hasinsqualitymaleharamservice.weebly.com\nRachel, Pawliuk, hasinsqualitymaleharamservice.weebly.com\nColin, Thompson, HondaCRV\nStuart, Seguin, HondaCRV\nVictor, Zheng, https://victorz.ca\nWentao, Sun, I Have No Friends\nApril, Jang, Jang Brothers\nMason, Brothers, Jang Brothers\nAlbert, Arrieta, Java-Chan\nGabrielle, Pili, Java-Chan\nNathan, Garcia, Java-Chan\nJenny, Zhang, Jenny Zhang\nJessie, Guo, Jessie Guo\nJohn, Kim, John Kim\nJongil, Yoon, Jongil Yoon\nJorge, Blanco, Jorge Blanco\nLeo, Gao, Leo Gao\nLiam, Wrubleski, Liam Wrubleski\nLogan, Pearce, Logan Pearce\nAdrian, Bathan, Lunatic Extra\nTeresa, Van, Lunatic Extra\nMahdi, Varposhti, Mahdi Varposhti\nMarc, Bondoc, Marc Bondoc\nMason, Hill, Mason Hill\nMichael, Qiu, Michael Qiu\nMichelle, Nguyen, Michelle Nguyen\nNihal, Potdar, nihalpot2002@gmail.com\nAlan, Fung, No Limit\nHaiyang, He, octowaffle\nJared, Pon, octowaffle\nPavan-Kumar, K-N, Pavan Kumar K N\nDiana, Mijangos, PROgrammers\nJaime, Wong, PROgrammers\nRohit, Kadhe, PROgrammers\nAvneet, Gill, Project Camel\nJofred, Cayabyab, Project Camel\nNavjot, Brar, Project Camel\nRadu, Schirliu, Radu Schirliu\nRoger, Chen, Roger Chen\nsam, robertson, sam robertson\nSarah, Walker, Sarah Walker\nSean, Oh, Sean Oh\nAndrew, Boisvert, Section-D\nCuong, Nguyen, Section-D\nSungwook, Baek, Section-D\nSifan, Xu, Sifan Xu\nGarth, Slaney, Slime\nJiho, Kim, Slime\nChristopher, Mossman, SQL Injection\nErik, Biederstadt, SQL Injection\nRichard, Pham, SQL Injection\nAnton, Lysov, Stack Overflow Coders\nChristopher, Behan, Stack Overflow Coders\nMontasir, Bechir, Stack Overflow Coders\nKyle, Storrier, struct by_lightning\nXin-Yan-(Jack), Xie, struct by_lightning\nAndrew, Burton, Tabs For A Better Tomorrow\nJordan, Labell, Tabs For A Better Tomorrow\nTyrell, Jaques, Tabs For A Better Tomorrow\nBritain-Van, Bergeyk, Tarantula Gang\nLeo, Nunez, Tarantula Gang\nQuenten, Welch, Tarantula Gang\nClark-Harrison, Dy, Team 404\nWenliang, Zheng, Team 404\nJane, Shen, Team Janey\nMustafa, Mukhlis, Team Janey\nRamiro, Piquer, Team Janey\nIden, Craven, Team NoName\nSteven, Hwang, Team NoName\nBenjamin, Messer, Team Pablo\nHarry, Chen, Team Pablo\nNathan, Enerson, Team Pablo\nAbid-Al, Labib, TEAM_B\nMd-Mamunur, Rashid, TEAM_B\nMushfik, Rahman, TEAM_B\nHien, Le, team7\nMINH, NGUYEN, team7\nJimmy, Deng, teamName\nMartin, Wong, teamName\nTyler, Starr, teamName\nAlex, Starr, teamName 2.0: The Better One\nEric, Huige, teamName 2.0: The Better One\nSanan, Rao, teamName 2.0: The Better One\nKim, Xie, Teem\nOmar, Abdelwahab, Teem\nVanessa, Kirchenberger, Teem\nIan, DeHaan, Terry Gannon Fan Club\nJacob, Skitsko, Terry Gannon Fan Club\nNoah, Gergel, Terry Gannon Fan Club\nCarlos, Hernandez, Text Here\nThai, Doan, Text Here\nXia, Liu, Text Here\nAndrew, Kett, The B#s\nLiam, Conway, The B#s\nAbdullah, Khan, The Immutables\nRichard, Norman, The Immutables\nShehab, Salem, The Immutables\nAbraham, Beauferris, The Outsiders\nJin, Sato, The Outsiders\nJoweria, Ekram, The Outsiders\nCristhian-Sotelo, Plaza, TheATeam\nKristopher, Lugtu, TheATeam\nThirusaanth, thanabal, TheATeam\nAbdullah, Al-Wadeiah, TripleA\nMinh, Tran, TripleA\nUsman, Zia, Usman Zia\nGrace, Nguyen, WCHS\nNgoc, Nguyen, WCHS\nAditya, Bandekar, WCHS Computing\nEric, Yang, WCHS Computing\nAustyn, Nagribianko, Win++\nDaine, McNiven, Win++\nChristopher, Schultze, XForce\nIshan, Vyas, XForce\nKevin, Sunkwa-Mills, XForce\nKevin, Zhang, Zero Gravity\nMaaz, Khurram, Zero Gravity";
+
+var ofSplit = officialContestants.split("\n");
+var ofArr = [];
+var winners = [];
+for (var i = 0; i < ofSplit.length; i++) {
+    var contestant = ofSplit[i].split(",");
+    ofArr.push([contestant[1], contestant[0], contestant[2]]);
+}
+ofArr.sort(function(a, b) {
+    if (a[1] < b[1]) return -1;
+    if (a[1] == b[1]) return a[0] < b[0] ? -1 : (a[0] > b[0] ? 1 : 0);
+    return 1;
+});
+var index;
+
+function writeContestants() {
+    var html_to_write = "";
+    for (var i = 0; i < ofArr.length; i++)
+	html_to_write += ofArr[i][1] + " " + ofArr[i][0] + ", ";
+    document.getElementById("remaining_contestants").innerHTML = html_to_write;
+}
+
+function draw() {
+    var button = document.getElementById("button");
+    button.disabled = true;
+    button.value = "And the winner is..."
+    var roulette = setInterval(
+	function() {
+	    index = Math.floor(Math.random() * (ofArr.length));
+	    document.getElementById("winner").innerHTML = ofArr[index][1] + " " + ofArr[index][0];
+	    document.getElementById("team").innerHTML = ofArr[index][2];
+	},
+	50);
+
+    setTimeout(
+	function() {
+	    clearInterval(roulette);
+	    index = Math.floor(Math.random() * (ofArr.length));
+	    document.getElementById("winner").innerHTML = ofArr[index][1] + " " + ofArr[index][0];
+	    document.getElementById("team").innerHTML = ofArr[index][2];
+	    ofArr.splice(index, 1);
+	    document.getElementById("contestant_count").innerHTML = "List of official contestants (" + ofArr.length.toString() + ")";
+	    writeContestants();
+	    document.getElementById("button").disabled = false;
+	    document.getElementById("button").value = "Start Draw";
+	},
+	3000);
+}
+document.getElementById("button").addEventListener("click", draw);
+
+writeContestants();
+document.getElementById("contestant_count").innerHTML = "List of official contestants (" + ofArr.length + "):";
