70e6f825148519dc0823f1b5f19b4d3115cec92d
Harsh Patil <harshnilkanth2015@gmail.com>
2021-11-26 15:16:47 -0700

    acpc2021/draw
    - Removed duplicate participants

--- contests/acpc/2021/readCSVFiles.py
+++ contests/acpc/2021/readCSVFiles.py
@@ -1,5 +1,7 @@
 import csv
 
+names = []
+
 with open('acpc_2021.csv', encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -8,6 +10,10 @@ with open('acpc_2021.csv', encoding="utf8") as csv_file:
             #print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            print(rf'\n{row[2]}, {row[4]}', end = "")
+            if row[2] in names:
+                pass
+            else:
+                print(rf'\n{row[2]}, {row[4]}', end = "")
+                names.append(row[2])
             line_count += 1
 
