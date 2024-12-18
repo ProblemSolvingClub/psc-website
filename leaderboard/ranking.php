ae624d8fc25a0da78cbf488b340ffee9bb094178
Caleb Cavilla <Caleb.Cavilla@gmail.com>
2024-12-17 23:10:55 -0700

    leaderboard bug fix #10

--- leaderboard/ranking.php
+++ leaderboard/ranking.php
@@ -13,7 +13,6 @@ $password = 'yWXvO{$ZSG,?';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Database connection successful.";
 
     // Fetch users
     $usersQuery = $pdo->query("SELECT firstName, lastName, totalSolved, attendedMeetings, bonusProblems FROM users");
