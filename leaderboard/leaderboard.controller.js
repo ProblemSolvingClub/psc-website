74af77172eb3e81843b31f0aaee6e1427272d68e
Caleb Cavilla <Caleb.Cavilla@gmail.com>
2024-12-17 22:01:05 -0700

    update leaderboard description

--- leaderboard/leaderboard.controller.js
+++ leaderboard/leaderboard.controller.js
@@ -22,7 +22,7 @@
                 vm.users = response.users.map(user => ({
                     ...user,
                     totalSolved: parseFloat(user.totalSolved),
-                    attendedMeetings: parseInt(user.attendedMeetings, 10),
+                    attendedMeetings: parseInt(user.attendedMeetings, 10)
                 }));
                 vm.sites = response.sites;
                 vm.tiers = response.tiers;
